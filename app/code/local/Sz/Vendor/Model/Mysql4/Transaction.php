e0cc1a0692a38351b79b515456973ca8470eb79a
maindev <dev@mypc.com>
2016-05-10 19:02:04 +0530

    removed sushil vendor module files

--- app/code/local/Sz/Vendor/Model/Mysql4/Transaction.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Sz_Vendor_Model_Mysql4_Transaction extends Mage_Core_Model_Mysql4_Abstract
-{
-    public function _construct()
-    {    
-        $this->_init('vendor/transaction', 'transid');
-    }
-}
--- app/code/local/Sz/Vendor/Model/Transaction.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Sz_Vendor_Model_Transaction extends Mage_Core_Model_Abstract
-{
-    public function _construct()
-    {
-        parent::_construct();
-        $this->_init('vendor/transaction');
-    }
-}
