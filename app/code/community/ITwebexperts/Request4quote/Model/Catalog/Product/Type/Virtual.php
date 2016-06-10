0a34151f89c567c1b62d3dd4f41f483c9f789726
maindev <dev@mypc.com>
2016-06-10 12:16:45 +0530

    adding Request for Quote extension

---
+++ app/code/community/ITwebexperts/Request4quote/Model/Catalog/Product/Type/Virtual.php
@@ -0,0 +1,33 @@
+<?php
+class ITwebexperts_Request4quote_Model_Catalog_Product_Type_Virtual extends Mage_Catalog_Model_Product_Type_Virtual {
+
+    protected function _prepareOptions(Varien_Object $buyRequest, $product, $processMode)
+    {
+        $transport = new StdClass;
+        $transport->options = array();
+        foreach ($this->getProduct($product)->getOptions() as $_option) {
+            /* @var $_option Mage_Catalog_Model_Product_Option */
+            $group = $_option->groupFactory($_option->getType())
+                ->setOption($_option)
+                ->setProduct($this->getProduct($product))
+                ->setRequest($buyRequest)
+                ->setProcessMode($processMode);
+            if(!$buyRequest->getData('r4q_quote_id')){
+                $group->validateUserValue($buyRequest->getOptions());
+            }
+
+            $preparedValue = $group->prepareForCart();
+            if ($preparedValue !== null) {
+                $transport->options[$_option->getId()] = $preparedValue;
+            }
+        }
+
+        $eventName = sprintf('catalog_product_type_prepare_%s_options', $processMode);
+        Mage::dispatchEvent($eventName, array(
+            'transport'   => $transport,
+            'buy_request' => $buyRequest,
+            'product' => $product
+        ));
+        return $transport->options;
+    }
+}
