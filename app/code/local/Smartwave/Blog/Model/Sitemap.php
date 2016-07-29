80ca74b3265c358092e15ecf83674a3ec101ebbd
maindev <dev@mypc.com>
2016-07-29 23:20:40 +0530

    sitemap generation format changes in the override file in blog module of porto - error corrections

--- app/code/local/Smartwave/Blog/Model/Sitemap.php
+++ app/code/local/Smartwave/Blog/Model/Sitemap.php
@@ -65,7 +65,7 @@ class Smartwave_Blog_Model_Sitemap extends Mage_Sitemap_Model_Sitemap
             }else{
                 $xml = sprintf(
                     '<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%.1f</priority></url>',
-                    htmlspecialchars($baseUrl . $item->getUrl()), substr(Mage::getModel('catalog/category')->load($item->getId())Mage::app()->getStore($storeId)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA)->getUpdated_at(),0,10), $changefreq, $priority
+                    htmlspecialchars($baseUrl . $item->getUrl()), substr(Mage::getModel('catalog/category')->load($item->getId())->getUpdated_at(),0,10), $changefreq, $priority
                 );
             }
             $this->sitemapFileAddLine($xml);
