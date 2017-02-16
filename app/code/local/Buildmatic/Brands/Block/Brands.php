a8dd6cd2f1712bb58e75a8f613ca7899c5d85131
user name <vinu.webtech@gmail.com>
2017-02-16 17:55:39 +0530

    corrections to brands module & checkout page login step double click error correction

--- app/code/local/Buildmatic/Brands/Block/Brands.php
+++ app/code/local/Buildmatic/Brands/Block/Brands.php
@@ -1,6 +1,10 @@
 <?php
 class Buildmatic_Brands_Block_Brands extends Mage_Core_Block_Template
 {
+    protected $_defaultToolbarBlock = 'catalog/product_list_toolbar';
+
+    protected $_productCollection;
+
     public function _prepareLayout() 
     {     
         $brand = Mage::getModel('buildmatic_brands/brands')->load($this->getRequest()->getParam('id'));    
@@ -9,11 +13,12 @@ class Buildmatic_Brands_Block_Brands extends Mage_Core_Block_Template
         $head->setKeywords($brand->getMetaKeyword());
         $head->setDescription($brand->getMetaDescription());
         $head->addLinkRel('canonical', Mage::getBaseUrl().$brand->getUrlKey());
+
         return parent::_prepareLayout();
     }
 
-	public function getBrand($brand_id)
-	{
+    public function getBrand($brand_id)
+    {
         if (Mage::registry('brand_data_id_collection'))
         {
             $brand = Mage::registry('brand_data_id_collection');
@@ -24,95 +29,174 @@ class Buildmatic_Brands_Block_Brands extends Mage_Core_Block_Template
             Mage::register('brand_data_id_collection', $brand);
         }
         return $brand;
-	}
-
-	public function getPriceHtml($product)
-    {
-        $this->setTemplate('brands/price.phtml');
-        $this->setProduct($product);
-        return $this->toHtml();
     }
 
-    public function getBrandProductCollection($brands)
-
+    public function getBrandProductCollection($brand)
     {
-
-
-            $category = Mage::getModel('catalog/category');
-
-            $tree = $category->getTreeModel();
-
-            $tree->load();
-
-            $ids = $tree->getCollection()->getAllIds();
-
-            if ($ids)
-
+        $layer = $this->getLayer();
+        $category = Mage::getModel('catalog/category');
+        $tree = $category->getTreeModel();
+        $tree->load();
+        $ids = $tree->getCollection()->getAllIds();
+        if ($ids)
+        {
+            $_productCollection = array();
+            foreach ($ids as $id)
             {
-
-                $new_id = 0;
-
-                foreach ($ids as $id)
-
+                $category->load($id);
+                if($category->getLevel()==2 && $category->getIsActive()==1)
                 {
-
-                    $category->load($id);
-
-                    if($category->getLevel()==2 && $category->getIsActive()==1)
-
+                    $productCollection = $category->getProductCollection();
+                    $attr = $productCollection->getResource()->getAttribute("manufacturer");
+                    if ($attr->usesSource())
                     {
-
-                        $productCollection = $category->getProductCollection();
-
-                        $attr = $productCollection->getResource()->getAttribute("manufacturer");
-
-                        if ($attr->usesSource())
-
-                        {
-
-                            $manufacturer_id = $attr->getSource()->getOptionId($brands->getBrandName());
-
-                        }
-
-                        $productCollection = $productCollection->addAttributeToSelect('*')->addAttributeToFilter('manufacturer', $manufacturer_id);
-
-                        if (!$_productCollection)
-
-                        {
-
-                            $_productCollection = $productCollection;
-
-                        }
-
-                        else
-
-                        {
-
-                            foreach ($productCollection as $item) 
-
-                            {
-
-                                $item->setId($new_id++);
-
-                                $_productCollection->addItem($item);
-
-                            }
-
-                        } 
-
+                        $manufacturer_id = $attr->getSource()->getOptionId($brand->getBrandName());
                     }
-
+                    $productCollection = $productCollection->addAttributeToSelect('*')->addAttributeToFilter('manufacturer', $manufacturer_id);
+                    if (count($_productCollection) == 0)
+                    {
+                        $_productCollection = $productCollection;
+                    }
+                    else
+                    {
+                        foreach ($productCollection as $item) 
+                        {
+                            $_productCollection->addItem($item);
+                        }
+                    }
                 }
-
-                Mage::register('brand_product_collection', $_productCollection);
-
-                var_dump(count($_productCollection));
-
-            }
-
-        
-
+            }  
+        }
+        //var_dump(count($_productCollection));
         return $_productCollection;
+    }
 
+    public function getCatProduct($category,$brand)
+    {
+        $layer = $this->getLayer();
+        $collection = Mage::getModel('catalog/category')
+                    ->load($category)
+                    ->getProductCollection();
+
+        $attr = $collection->getResource()->getAttribute("manufacturer");
+        if ($attr->usesSource()) 
+        {
+            $manufacturer_id = $attr->getSource()->getOptionId($brand->getBrandName());
+        }
+        $collection = $collection->addAttributeToSelect('*')->addAttributeToFilter('manufacturer', $manufacturer_id); 
+        return $collection;
+    }
+
+    protected function _beforeToHtml()
+    {
+        $toolbar = $this->getToolbarBlock();
+        //var_dump($toolbar);
+        
+        // called prepare sortable parameters
+        $collection = $this->_getProductCollection();
+
+        // use sortable parameters
+        if ($orders = $this->getAvailableOrders()) 
+        {
+            $toolbar->setAvailableOrders($orders);
+        }
+        if ($sort = $this->getSortBy()) 
+        {
+            $toolbar->setDefaultOrder($sort);
+        }
+        if ($dir = $this->getDefaultDirection()) 
+        {
+            $toolbar->setDefaultDirection($dir);
+        }
+        if ($modes = $this->getModes()) 
+        {
+            $toolbar->setModes($modes);
+        }
+
+        // set collection to toolbar and apply sort
+        $toolbar->setCollection($collection->clear());
+
+        $this->setChild('toolbar', $toolbar);
+        Mage::dispatchEvent('catalog_block_product_list_collection', array(
+            'collection' => $collection
+        ));
+
+        //$collection->load();
+
+        return parent::_beforeToHtml();
+    }
+
+    public function getToolbarBlock()
+    {
+        if ($blockName = $this->getToolbarBlockName()) 
+        {
+            if ($block = $this->getLayout()->getBlock($blockName)) 
+            {
+                return $block;
+            }
+        }
+        $block = $this->getLayout()->createBlock($this->_defaultToolbarBlock, microtime());
+        return $block;
+    }
+
+    public function getToolbarHtml()
+    {
+        return $this->getChildHtml('toolbar');
+    }
+
+    public function getMode()
+    {
+        return $this->getChild('toolbar')->getCurrentMode();
+    }
+
+    public function setCollection($collection)
+    {
+        $this->_productCollection = $collection;
+        return $this;
+    }
+
+    public function getLayer()
+    {
+        $layer = Mage::registry('current_layer');
+        if ($layer) {
+            return $layer;
+        }
+        return Mage::getSingleton('catalog/layer');
+    }
+
+    public function getLoadedProductCollection()
+    {
+        return $this->_getProductCollection();
+    }
+
+    protected function _getProductCollection()
+    {
+        if (is_null($this->_productCollection)) 
+        {
+            $layer = $this->getLayer();
+            $request = Mage::app()->getRequest();
+            $parts = explode('/', $request->getPathInfo());
+            foreach ($parts as $part) 
+            {
+                if ($part != "") 
+                {
+                    $catName = $part;
+                }
+            }
+            $brand = $this->getBrand($this->getRequest()->getParam('id'));
+            if ($catName == $brand->getUrlKey())
+            {
+                $this->_productCollection = $this->getBrandProductCollection($brand);
+            }
+            else
+            {
+                $category = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addAttributeToFilter('url_key', $catName);
+                $data = $category->getData();
+                $data = $data['0']['entity_id'];
+
+                $this->_productCollection = $this->getCatProduct($data,$brand);
+            }
+        }
+        return $this->_productCollection;
     }
 }
--- app/code/local/Buildmatic/Brands/Controller/Router.php
+++ app/code/local/Buildmatic/Brands/Controller/Router.php
@@ -20,27 +20,27 @@ class Buildmatic_Brands_Controller_Router extends Mage_Core_Controller_Varien_Ro
 
         $urlKey = trim($request->getPathInfo(), '/');
         $parts = explode('/', $urlKey);
-        if (count($parts) == 1)
+        if (count($parts) == 2)
         {
             $check = array(); //you can add here multiple entities
             $check['buildmatic_brands/brands'] = new Varien_Object(array(
-                //'prefix'        => Mage::getStoreConfig('brands_section/brands_Settings/url_prefix'),
+                'prefix'        => Mage::getStoreConfig('brands_section/brands_settings/url_prefix'),
                 'model'         =>'buildmatic_brands/brands',
                 'controller'    => 'brands',
                 'action'        => 'view',
                 'param'         => 'id',
             ));
             foreach ($check as $key=>$settings) {
-                /*if ($settings['prefix']){
+                if ($settings['prefix']){
                     $parts = explode('/', $urlKey);
                     if ($parts[0] != $settings['prefix'] || count($parts) != 2){
                         continue;
                     }
-                    $urlKey = $parts[1];
-                }*/
+                    $urlKey1 = $parts[1];
+                }
                 //$model = Mage::getModel($settings->getModel());
                 $model = Mage::getModel('buildmatic_brands/brands');
-                $checkid = $model->getCollection()->addFieldToFilter('url_key',$urlKey)->addFieldToSelect('id');
+                $checkid = $model->getCollection()->addFieldToFilter('url_key',$urlKey1)->addFieldToSelect('id');
                 $id = $checkid->getData();
                 if ($id){
                     if ($settings->getCheckPath() && !$model->load($id)->getStatusPath()) {
@@ -58,25 +58,30 @@ class Buildmatic_Brands_Controller_Router extends Mage_Core_Controller_Varien_Ro
                 }
             }
         }else{
+            $check = array();
             $check['buildmatic_brands/brands/category'] = new Varien_Object(array(
+                'prefix'        => Mage::getStoreConfig('brands_section/brands_settings/url_prefix'),
                 'model'         =>'buildmatic_brands/brands',
                 'controller'    => 'brands',
                 'action'        => 'viewCategory',
                 'param'         => 'id',
             ));
             foreach ($check as $key=>$settings) {
-                
+                if ($settings['prefix']){
                     $parts = explode('/', $urlKey);
-                    $urlKey = $parts[0];
-                    $categoryName = $parts[2];
-                    
+                    if ($parts[0] != $settings['prefix'] || count($parts) != 4){
+                        continue;
+                    }
+                    $urlKey1 = $parts[1];
+                    $categoryName = $parts[4];
+                }
                 //$model = Mage::getModel($settings->getModel());
                 $categoryModel = Mage::getModel('catalog/category');
                 $catId = $categoryModel->getCollection()->addAttributeToFilter('name',$categoryName)->addAttributeToSelect('entity_id');
                 if($catId){
 
                     $model = Mage::getModel('buildmatic_brands/brands');
-                    $checkid = $model->getCollection()->addFieldToFilter('url_key',$urlKey)->addFieldToSelect('id');
+                    $checkid = $model->getCollection()->addFieldToFilter('url_key',$urlKey1)->addFieldToSelect('id');
                     $id = $checkid->getData();
                     if ($id){
                         if ($settings->getCheckPath() && !$model->load($id)->getStatusPath()) {
